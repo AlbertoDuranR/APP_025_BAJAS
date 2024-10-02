5457de19bec5c5c76ebddbf48ac2dde97ea0db32
AlbertoDuranR <teogenes.duran@terranovatrading.com.pe>
2024-10-02 08:37:06 -0500

    se configura el inicio y el readme

--- app.py
+++ app.py
@@ -10,5 +10,10 @@ cors = CORS(app, resources={r"/*": {"origins": "*"}})
 # registramos los Blueprints
 app.register_blueprint(low, url_prefix='/low')
 
+# Definir una ruta de redireccionamiento por defecto a '/low'
+@app.route('/')
+def default():
+    return redirect('/low')
+
 if __name__ == '__main__':
     app.run(port=8007,  debug=True)
---
+++ readme.md
@@ -0,0 +1,13 @@
+# construir
+docker build -t app_bajas .
+
+# levantar conenedor
+docker run -d -p 5000:5000 --name app_bajas_container app_bajas
+
+# ver logs
+docker logs app_bajas_container
+
+# detener contenedor
+docker stop app_bajas_container
+
+
