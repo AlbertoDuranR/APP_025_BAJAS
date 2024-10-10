8a085eb3068694a311254a7df6762b41fa58f6d7
AlbertoDuranR <teogenes.duran@terranovatrading.com.pe>
2024-10-10 16:35:34 -0500

    añade rutas para la cookie

--- routes/sunatRoutes.py
+++ routes/sunatRoutes.py
@@ -1,6 +1,10 @@
 from flask import Blueprint, render_template, request, send_file
 from utils.responseBuilder import responseBuilder
 from models.sunatModel import SunatValidator
+import json, os
+from datetime import datetime 
+import pytz
+
 
 sunat = Blueprint('sunat', __name__)
 
@@ -25,3 +29,56 @@ def validate():
 
     except Exception as e:
         return responseBuilder.error(f"Ocurrió un error al procesar los archivos: {str(e)}")
+    
+
+@sunat.route('/updateToken', methods=['POST'])
+def updateToken():
+    token = request.form.get('token')
+    
+    if not token:
+        return responseBuilder.error('No se especificó un token válido')
+
+    try:
+        # Definir la ruta del archivo cookie.json
+        cookie_file_path = os.path.join('token', 'cookie.json')
+
+        # Leer el archivo JSON actual
+        with open(cookie_file_path, 'r') as file:
+            cookie_data = json.load(file)
+
+        # Obtener la zona horaria de Perú
+        peru_tz = pytz.timezone('America/Lima')
+        
+        # Obtener la fecha y hora actual en la zona horaria de Perú
+        peru_time = datetime.now(peru_tz).strftime('%Y-%m-%dT%H:%M:%S')
+
+        # Actualizar el campo "cookie" y "dateTime"
+        cookie_data['cookie'] = token
+        cookie_data['dateTime'] = peru_time
+
+        # Guardar los cambios en el archivo JSON
+        with open(cookie_file_path, 'w') as file:
+            json.dump(cookie_data, file, indent=4)
+
+        return responseBuilder.success('Token actualizado correctamente')
+
+    except Exception as e:
+        return responseBuilder.error(f"Ocurrió un error al actualizar el token: {str(e)}")
+    
+
+
+@sunat.route('/getToken', methods=['GET'])
+def getToken():
+    try:
+        # Definir la ruta del archivo cookie.json
+        cookie_file_path = os.path.join('token', 'cookie.json')
+
+        # Leer el archivo JSON actual
+        with open(cookie_file_path, 'r') as file:
+            cookie_data = json.load(file)
+
+        # Devolver la información de la cookie
+        return responseBuilder.success(cookie_data)
+
+    except Exception as e:
+        return responseBuilder.error(f"Ocurrió un error al obtener el token: {str(e)}")
